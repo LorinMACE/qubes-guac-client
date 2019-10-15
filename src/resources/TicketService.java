de9c687acf85cf5b01c87811c3e67e19b5813334
LorinMACE <LorinMACE@users.noreply.github.com>
2019-10-15 22:44:27 +0200

    Apply Fepitre gist

--- src/resources/TicketService.java
+++ src/resources/TicketService.java
@@ -1,5 +1,8 @@
 package resources;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,16 +21,54 @@ public class TicketService extends GuacamoleHTTPTunnelServlet {
 	private static final long serialVersionUID = 3493778309036478051L;
 	List<String> allowed_protocols = Arrays.asList("ssh", "vnc");
 	
+	public String executeCommand(String command) {
+		Process p;
+		String s = null;
+		String result = "";
+		try {
+			p = Runtime.getRuntime().exec(command);
+
+			
+	        BufferedReader stdInput = new BufferedReader(new 
+	             InputStreamReader(p.getInputStream()));
+	
+	        BufferedReader stdError = new BufferedReader(new 
+	             InputStreamReader(p.getErrorStream()));
+	
+	        while ((s = stdInput.readLine()) != null) {
+	        	result += s;
+	        }
+	        
+	        while ((s = stdError.readLine()) != null) {
+	        	result += s;
+	        }
+	       
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public String preparePort (String vmame, String service) {
+		String command = String.format("/home/user/QubesIncomming/work-lade/remote-session.py open %s %s", "vmname", "service");
+		executeCommand(command);
+        
+		command = String.format("/home/user/QubesIncomming/work-lade/remote-session.py get %s %s", "vmname", "service");
+		return executeCommand(command);
+	}
+	
 	@Override
     protected GuacamoleTunnel doConnect(HttpServletRequest request) throws GuacamoleException {
 
 
-		String port = request.getHeader("port");
+		String vm = request.getHeader("vm");
 		String protocol = request.getHeader("protocol");
 
 		if (!allowed_protocols.contains(protocol)) {
 	        return null;
 	    }
+		String port = preparePort(vm, protocol);
 
         GuacamoleConfiguration config = new GuacamoleConfiguration();
         config.setProtocol(protocol);
